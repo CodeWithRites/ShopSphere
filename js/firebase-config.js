5f520f9ed1d97a819cabd021f34bb8d6ea5321fa
Ritesh Gouda <goudaritesh785@gmail.com>
2026-03-31 10:38:35 +0530

    Update Firebase configuration with placeholders
    Replaced Firebase configuration values with placeholders.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,11 @@
 window.ShopSphereFirebaseConfig = {
-  apiKey: "AIzaSyB8QAu_zP6sGOMuQJClQ3yGcO1gkLFnZyQ",
-  authDomain: "shopsphere-82fe8.firebaseapp.com",
-  projectId: "shopsphere-82fe8",
-  storageBucket: "shopsphere-82fe8.firebasestorage.app",
-  messagingSenderId: "342157861200",
-  appId: "1:342157861200:web:32be939e5d1f75a3f58c55",
-  measurementId: "G-L062F9G8G2"
+  apiKey: "xxxxxx",
+  authDomain: "xxxxxx",
+  projectId: "xxxxxx",
+  storageBucket: "xxxxxx",
+  messagingSenderId: "xxxxxx",
+  appId: "xxxxxxx",
+  measurementId: "xxxxxxxxx"
 };
+
+//Add Own Firebase ID
